bc0cfade2d93665ca2503f8aae4089117b49bb6d
Saketh <saketh.varma@redaptiveinc.com>
2022-06-29 18:57:41 -0700

    Palettes from API. Pull down to refresh

--- screens/Home.js
+++ screens/Home.js
@@ -2,51 +2,35 @@ import React from 'react';
 import {FlatList, StyleSheet} from 'react-native';
 import PalettePreview from '../components/PalettePreview';
 
-const SOLARIZED = [
-  {colorName: 'Base03', hexCode: '#002b36'},
-  {colorName: 'Base02', hexCode: '#073642'},
-  {colorName: 'Base01', hexCode: '#586e75'},
-  {colorName: 'Base00', hexCode: '#657b83'},
-  {colorName: 'Base0', hexCode: '#839496'},
-  {colorName: 'Base1', hexCode: '#93a1a1'},
-  {colorName: 'Base2', hexCode: '#eee8d5'},
-  {colorName: 'Base3', hexCode: '#fdf6e3'},
-  {colorName: 'Yellow', hexCode: '#b58900'},
-  {colorName: 'Orange', hexCode: '#cb4b16'},
-  {colorName: 'Red', hexCode: '#dc322f'},
-  {colorName: 'Magenta', hexCode: '#d33682'},
-  {colorName: 'Violet', hexCode: '#6c71c4'},
-  {colorName: 'Blue', hexCode: '#268bd2'},
-  {colorName: 'Cyan', hexCode: '#2aa198'},
-  {colorName: 'Green', hexCode: '#859900'},
-];
-
-const RAINBOW = [
-  {colorName: 'Red', hexCode: '#FF0000'},
-  {colorName: 'Orange', hexCode: '#FF7F00'},
-  {colorName: 'Yellow', hexCode: '#FFFF00'},
-  {colorName: 'Green', hexCode: '#00FF00'},
-  {colorName: 'Violet', hexCode: '#8B00FF'},
-];
-
-const FRONTEND_MASTERS = [
-  {colorName: 'Red', hexCode: '#c02d28'},
-  {colorName: 'Black', hexCode: '#3e3e3e'},
-  {colorName: 'Grey', hexCode: '#8a8a8a'},
-  {colorName: 'White', hexCode: '#ffffff'},
-  {colorName: 'Orange', hexCode: '#e66225'},
-];
-
-const COLOR_PALETTES = [
-  {paletteName: 'Solarized', colors: SOLARIZED},
-  {paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS},
-  {paletteName: 'Rainbow', colors: RAINBOW},
-];
-
 const Home = ({navigation}) => {
+  const [palettes, setPalettes] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleFetchPalettes = React.useCallback(async () => {
+    const response = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes',
+    );
+    if (response.ok) {
+      const palettesFromApi = await response.json();
+      setPalettes(palettesFromApi);
+    }
+  }, []);
+
+  const handleRefresh = React.useCallback(async () => {
+    setIsLoading(true);
+    await handleFetchPalettes();
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, [handleFetchPalettes]);
+
+  React.useEffect(() => {
+    handleFetchPalettes();
+  });
+
   return (
     <FlatList
-      data={COLOR_PALETTES}
+      data={palettes}
       style={styles.list}
       keyExtractor={item => item.paletteName}
       renderItem={({item}) => (
@@ -55,6 +39,8 @@ const Home = ({navigation}) => {
           colorPalette={item}
         />
       )}
+      refreshing={isLoading}
+      onRefresh={handleRefresh}
     />
   );
 };
